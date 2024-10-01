33863145edbb3716dbc474fcedb3aeff2edc46a7
Gen346 <kelzreez@gmail.com>
2024-10-01 11:21:06 +0300

    Add project files.

---
+++ Лабораторна робота 10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string of words separated by spaces:");
+        string inputString = Console.ReadLine();
+
+        int numberOfWords = CountWordsStartingWithVowel(inputString);
+
+        Console.WriteLine("Number of words starting with a vowel: " + numberOfWords);
+    }
+
+    static int CountWordsStartingWithVowel(string inputString)
+    {
+        string[] words = inputString.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        int count = 0;
+
+        foreach (string word in words)
+        {
+            char firstLetter = word[0];
+            if (IsVowel(firstLetter))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static bool IsVowel(char letter)
+    {
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u'};
+        return Array.Exists(vowels, v => v == letter);
+    }
+}
